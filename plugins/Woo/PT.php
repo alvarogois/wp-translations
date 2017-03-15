d7c9ff36fb9666e24f0079596c4370e69bd53f3b
Alvaro Gois dos Santos <alvarogois@BatBookSSD.local>
2017-03-15 12:11:06 +0000

    Update trunk
    Lots of translations made over the years, some are too old and should
    be disregarded. Updating soon.

---
+++ plugins/Woo/PT.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Portugal states
+ *
+ * @author   	Marco Almeida
+ * @category 	i18n
+ * @package 	WooCommerce/i18n
+ * @version     2.2.3
+ */
+global $states;
+
+$states["PT"] = array(
+	'AC' => __('Açores',			'woocommerce'),
+	'AV' => __('Aveiro',			'woocommerce'),
+	'BJ' => __('Beja',				'woocommerce'),
+	'BR' => __('Braga',				'woocommerce'),
+	'BG' => __('Bragança',			'woocommerce'),
+	'CB' => __('Castelo Branco',	'woocommerce'),
+	'CM' => __('Coimbra',			'woocommerce'),
+	'EV' => __('Évora',				'woocommerce'),
+	'FR' => __('Faro',				'woocommerce'),
+	'GD' => __('Guarda',			'woocommerce'),
+	'LR' => __('Leiria',			'woocommerce'),
+	'LS' => __('Lisboa',			'woocommerce'),
+	'MD' => __('Madeira',			'woocommerce'),
+	'PR' => __('Portalegre',		'woocommerce'),
+	'PT' => __('Porto',				'woocommerce'),
+	'ST' => __('Santarém',			'woocommerce'),
+	'SB' => __('Setúbal', 			'woocommerce'),
+	'VC' => __('Viana do Castelo',	'woocommerce'),
+	'VR' => __('Vila Real',			'woocommerce'),
+	'VS' => __('Viseu',				'woocommerce'),
+);
